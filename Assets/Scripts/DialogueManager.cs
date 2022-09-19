41203201569a8038b53f0b96fbe6f3e6f8a1c170
Matthew Wierenga <matt.wierenga@outlook.com>
2022-09-19 11:10:14 +0200

    Added DialogueManager for the dialogue

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : ScriptableObject
+
+
+{
+    Queue<string> DialogueCheckpointRace = new Queue<string>();
+    Queue<string> DialogueBeginnerRace = new Queue<string>();
+    Queue<string> DialogueAdvancedRace = new Queue<string>();
+
+
+
+
+
+}
