60dcce5eac619d596bfa9b8dddd15ff50627ebb2
DustyRusks <88307813+DustyRusks@users.noreply.github.com>
2022-09-20 15:19:31 +0200

    Fixed stack dialogue, added main menu

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -3,16 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     public Text TextChanger;
     public GameObject btnNext;
+    [SerializeField] private Queue<string> checkpointRaceDialogue;
+    [SerializeField] private Queue<string> beginnerRaceDialogue;
+    [SerializeField] private Queue<string> advancedRaceDialogue;
+    [SerializeField] private Queue<string> expertDialogue;
+    [SerializeField] private TextMeshProUGUI dialogueDisplay;
+
+    public void nextDialogue()
+    {
+        checkpointRaceDialogue = new Queue<string>();
+        checkpointRaceDialogue.Enqueue("Hello Challenger");
+        checkpointRaceDialogue.Enqueue("Today, you will be taking on the Checkpoint Race");
+        checkpointRaceDialogue.Enqueue("This race is to test your skills with the vehicle");
+        checkpointRaceDialogue.Enqueue("You will need to race through the checkpoints to gain more time around the course");
+        checkpointRaceDialogue.Enqueue("If you run out of time, you will lose the race");
+        checkpointRaceDialogue.Enqueue("So do not waste time while going aroudnd the course. Good Luck!");
 
 
+        beginnerRaceDialogue = new Queue<string>();
+        beginnerRaceDialogue.Enqueue("Hello Challenger");
+        beginnerRaceDialogue.Enqueue("Today, you will be taking on the Beginner Race");
+        beginnerRaceDialogue.Enqueue("This race is to test your skills with the vehicle and how you face againsts an opponent");
+        beginnerRaceDialogue.Enqueue("You will need to race through the course, passing checkpoints and try and beat your opponent");
+        beginnerRaceDialogue.Enqueue("If you do not beat the opponent, you will not be able to advance through the game");
+        beginnerRaceDialogue.Enqueue("So do not waste time while going aroudnd the course. Good Luck!");
+
+        advancedRaceDialogue = new Queue<string>();
+
+        advancedRaceDialogue.Enqueue("Hello Challenger");
+        advancedRaceDialogue.Enqueue("This part of the game is not implemented.");
+        advancedRaceDialogue.Enqueue("So go away.");
+
+    }
+    public void nextConvo()
+    {
+        //print(dialogue1.Peek());
+        
+
+        if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("DialogueForCheckpointRace"))
+        {
+            dialogueDisplay.text = checkpointRaceDialogue.Peek();
+            Debug.Log("You are in checkpointRace");
+
+
+
+        }
+        else
+            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("DialogueForBeginnerRace"))
+        {
+            dialogueDisplay.text = beginnerRaceDialogue.Dequeue().ToString();
+
+            Debug.Log("You are in BeginnerRace");
+
+
+        }
+        else
+            if (SceneManager.GetActiveScene() == SceneManager.GetSceneByName("DialogueForAdvancedRace"))
+        {
+            dialogueDisplay.text = advancedRaceDialogue.Dequeue().ToString();
+            Debug.Log("You are in AdvancedRace");
+
+
+        }
+
+        checkpointRaceDialogue.Dequeue();
+    }
     public void Start()
     {
-
+        nextDialogue();
         //adding the dialogue for the queues
         Queue CheckpointRace = new Queue();
         CheckpointRace.Enqueue("Hello Challenger");
